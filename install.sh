287a4891686d20cc9bb78c5939ae67f04e2a360c
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2022-11-10 01:56:57 +0000

    FIx p10k config

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # zsh
 ln -sf ${BASEDIR}/.zshrc ~/.zshrc
-ln -sf ${BASEDIR}/p10k.zsh ~/.p10k.zsh
+ln -sf ${BASEDIR}/.p10k.zsh ~/.p10k.zsh
 ln -sf ${BASEDIR}/.zsh ~/.zsh
 
 # git
