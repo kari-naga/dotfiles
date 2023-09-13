47e199170987ed304e5d4d58e150698382174f05
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2023-09-13 11:34:17 -0400

    Update dotfiles

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ln -sf ${BASEDIR}/.zshrc ~/.zshrc
 ln -sf ${BASEDIR}/.p10k.zsh ~/.p10k.zsh
 ln -sf ${BASEDIR}/.zsh ~/.zsh
+sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 
 # git
 ln -sf ${BASEDIR}/.gitconfig ~/.gitconfig
